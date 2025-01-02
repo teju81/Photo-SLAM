e6caa8a9c27e5c0df93238a66865f6fe19dd1ab3
teju81 <teju81@gmail.com>
2025-01-02 20:00:40 +0530

    Create run_tum_rgbd.sh

---
+++ scripts/run_tum_rgbd.sh
@@ -0,0 +1,13 @@
+tum_dataset=$1
+
+if ["${tum_dataset}" == "fr1_desk"]; then
+  ../bin/tum_rgbd \
+  ../ORB-SLAM3/Vocabulary/ORBvoc.txt \
+  ../cfg/ORB_SLAM3/RGB-D/TUM/tum_freiburg1_desk.yaml \
+  ../cfg/gaussian_mapper/RGB-D/TUM/tum_freiburg1_desk.yaml \
+  /root/code/datasets/tum/rgbd_dataset_freiburg1_desk \
+  /root/code/datasets/tum/rgbd_dataset_freiburg1_desk/association.txt \
+  /root/code/output/
+else
+  echo "Usage: run_tum_rgbd.sh $tum_dataset" 
+fi
